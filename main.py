3dd1aedb6ca2c42e62231650cebc1111b6e2b5d7
Ansh sharma <anshshama8nbi@gmail.com>
2025-05-01 16:00:06 +0530

    Taking input from user

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 #this is the phase First taking the input from the user and then passing it to the model
+'''
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+PHASE 1: Taking input from the user and passing it to the model
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+'''
+
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 load_dotenv()
 
 model = ChatGoogleGenerativeAI(model= 'gemini-1.5-pro')
 
-result = model.invoke("What is the capital of France?")
-print(result.content)
+user = input("Enter the Issue: ")
+
+result = model.invoke(user)
+# print("Model response:")
+print("Model response:" ,result.content)
